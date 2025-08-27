116daf0588f1a62ed1da8d9490278d4c4f3d0d02
Nitya Santhosh <135213405+nityasanthosh@users.noreply.github.com>
2025-08-27 11:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (26.11%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,21 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int single = 0;
+//         for(int i=0; i<nums.size(); i++){
+//             single = single ^ nums[i];
+
+//         }return single;
+        
+//     }
+// };
+
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int single = 0;
-        for(int i=0; i<nums.size(); i++){
-            single = single ^ nums[i];
-
-        }return single;
-        
+    int singleNumber(vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
     }
 };
